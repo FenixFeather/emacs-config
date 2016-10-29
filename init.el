19669960a4ac39b4c2e967f1e4c588cac6b9e581
Thomas Liu <thomasliu02@gmail.com>
2016-10-29 15:16:46 -0500

    Try to improve web mode

--- init.el
+++ init.el
@@ -9,6 +9,8 @@
 (add-to-list 'load-path "~/.emacs.d/lisp/ess-13.09-1/lisp")
 (load "ess-site")
 
+(setq scroll-preserve-screen-position t)
+
 ;;Windows specific
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex) 
 (setq reftex-plug-into-AUCTeX t)
@@ -190,6 +192,29 @@
 (defun mp-add-cpp-keys()
   (local-set-key (kbd "C-.") 'insert-arrow)
   (local-set-key (kbd "C-c C-k" ) 'uncomment-region))
+
+;; jsx Stuff
+;; (flycheck-define-checker jsxhint-checker
+;;   "A JSX syntax and style checker based on JSXHint."
+
+;;   :command ("jsxhint" source)
+;;   :error-patterns
+;;   ((error line-start (1+ nonl) ": line " line ", col " column ", " (message) line-end))
+;;   :modes (web-mode))
+
+;; (add-hook 'web-mode-hook
+;;           (lambda ()
+;;             (when (equal web-mode-content-type "jsx")
+;;               ;; enable flycheck
+;;               (flycheck-select-checker 'jsxhint-checker)
+;;               (flycheck-mode))))
+
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+      (let ((web-mode-enable-part-face nil))
+        ad-do-it)
+    ad-do-it))
 
 ;;LaTeX Stuff
 ;; (setq TeX-auto-save t)
